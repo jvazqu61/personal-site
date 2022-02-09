db22d82b941b43df7c4239211fbf0821d0e12796
Jvazqu61 <jvazqu61@uic.edu>
2022-02-09 09:36:00 -0600

    init for site

---
+++ components/NavBar.js
@@ -0,0 +1,14 @@
+
+function NavBar() {
+  return (
+    <nav>
+        <h1>Juan Vazquez</h1>
+        <l>
+            <li>Git</li>
+            <li>Resume</li>
+        </l>
+    </nav>
+  )
+}
+
+export default NavBar
