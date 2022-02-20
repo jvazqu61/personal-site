c9530ffcbc6fb36dda41e9264a15a970aca2f32d
Jvazqu61 <jvazqu61@uic.edu>
2022-02-20 15:15:38 -0600

    refactored nav bar

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,30 +1,37 @@
 import Link from 'next/link';
 import styles from '../styles/NavBar.module.css';
 import { BsGithub, BsFillFileEarmarkPersonFill} from 'react-icons/bs';
+import {GiHamburgerMenu} from 'react-icons/gi';
+import { CgClose } from 'react-icons/cg';
 import Resume from '../public/resume.pdf';
+import { useState } from 'react';
 
 function NavBar() {
+
+    const [isToggled, setIsToggled] = useState(false);
+
+    const toggleMenu = () =>{
+        setIsToggled(!isToggled);
+    }
+
   return (
-      <header className={styles.header}>
-        <a className={styles.title}>Juan Vazquez</a>
-        <nav className={styles.navcenter}>
-        <ul >
+
+    <nav className={styles.navbar}>
+        <div className={styles.title}>Juan Vazquez</div>
+        {isToggled?<CgClose className={styles.menu_icon}
+            onClick={toggleMenu}/>: <GiHamburgerMenu 
+            className={styles.menu_icon}
+            onClick={toggleMenu} />}
+        <div className={isToggled?`${styles.nav_links} ${styles.links_active}`: styles.nav_links} >
+            <ul >
                 <li><a href='#about'>About</a></li>
                 <li><a href='#skills'>Skills</a></li>
                 <li><a href='#projects'>Projects</a></li>
+                <li><a href='https://github.com/jvazqu61'><BsGithub style={{fontSize:'30px'}}/></a></li>
+                 <li><a href='../public/resume.pdf'download><BsFillFileEarmarkPersonFill style={{fontSize:'30px'}}/></a></li>
             </ul>
-        </nav>
-        <nav className={styles.navright}>
-            <ul >
-                <li>
-                    <a href='https://github.com/jvazqu61'><BsGithub style={{fontSize:'35px'}}/></a>
-                </li>
-                <li>
-                    <a href='../public/resume.pdf'download><BsFillFileEarmarkPersonFill style={{fontSize:'35px'}}/></a>
-                </li>
-            </ul>
-        </nav> 
-      </header>
+        </div>
+    </nav>
     
   )
 }
